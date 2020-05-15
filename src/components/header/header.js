4b885fd0c7b3433bac0f5f7c811fde0e09f0f696
Brandon Cantello <bcantello@gmail.com>
2020-05-14 18:26:36 -0700

    updates logo and color scheme for all pages

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,18 @@
 import React from "react";
+import colorlogo from "../../media/colorLogo.png"
 import './header.css'
 
 export default function Header() {
 	return (
 		<header className="blog-header py-3">
 			<div className={"header-button-container"}>
+				<img src={colorlogo}/>
 				<a className="btn btn-sm btn-outline-secondary" href="/home">Home</a>
 				<a className="btn btn-sm btn-outline-secondary" href="/login">Log in</a>
 				<a className="btn btn-sm btn-outline-secondary" href="/signup">Sign up</a>
 				<a className="btn btn-sm btn-outline-secondary" href="/create-activity">Post activity</a>
 			</div>
+			{/*<img src={colorlogo}/>*/}
 		</header>
 	);
 };
